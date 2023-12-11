0eb8d1b9da17cecdd84ac78fa7fdecff4bad6f05
victorlee <victor_lee109@hotmail.co.uk>
2023-12-11 19:14:59 +0000

    pages linked

---
+++ assets/js/logic.js
@@ -0,0 +1,117 @@
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+function startQuiz() {
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+  questionsEl.removeAttribute("class");
+  timerId = setInterval(clockTick, 1000);
+  timerEl.textContent = time;
+
+  getQuestion();
+}
+
+function getQuestion() {
+  var currentQuestion = questions[currentQuestionIndex];
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+  choicesEl.innerHTML = "";
+  currentQuestion.choices.forEach(function(choice, i) {
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+    choiceNode.textContent = i + 1 + ". " + choice;
+    choiceNode.onclick = questionClick;
+    choicesEl.appendChild(choiceNode);
+  });
+}
+
+function questionClick() {
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    time -= 15;
+
+    if (time < 0) {
+      time = 0;
+    }
+    timerEl.textContent = time;
+    feedbackEl.textContent = "Incorrect!";
+    feedbackEl.style.color = "red";
+    feedbackEl.style.fontSize = "100%";
+  } else {
+    feedbackEl.textContent = "Correct!";
+    feedbackEl.style.color = "green";
+    feedbackEl.style.fontSize = "100%";
+  }
+
+  feedbackEl.setAttribute("class", "feedback");
+  setTimeout(function() {
+    feedbackEl.setAttribute("class", "feedback hide");
+  }, 1000);
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+function quizEnd() {
+  clearInterval(timerId);
+
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
+
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
+
+  questionsEl.setAttribute("class", "hide");
+}
+
+function clockTick() {
+  time--;
+  timerEl.textContent = time;
+
+  if (time <= 0) {
+    quizEnd();
+  }
+}
+
+function saveHighscore() {
+  var initials = initialsEl.value.trim();
+
+  if (initials !== "") {
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    window.location.href = "score.html";
+  }
+}
+
+function checkForEnter(event) {
+  if (event.key === "Enter") {
+    saveHighscore();
+  }
+}
+
+submitBtn.onclick = saveHighscore;
+
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+var questions = [
+  {
+    title: "What does 'JS' stand for",
+    choices: ["JavaServer", "JavaScript", "JSON", "Java Source"],
+    answer: "JavaScript",
+  },
+  {
+    title: "Which keyword is used to declare a variable in JavaScript?",
+    choices: ["var", "let", "const", "int"],
+    answer: "var",
+  },
+  {
+    title: "What is the purpose of the 'addEventListener' method?",
+    choices: [
+      "To add two numbers",
+      "To handle user actions",
+      "To create an element",
+      "To define a class",
+    ],
+    answer: "To handle user actions",
+  },
+  {
+    title:
+      "What will the following code log to the console? console.log(typeof [])",
+    choices: ["Array", "Object", "undefined", "number"],
+    answer: "Object",
+  },
+];
